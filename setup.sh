5bad80385d7bc8a38ded36e25e081bc987a56a3c
Kamlesh <32892726+kamleshpawar17@users.noreply.github.com>
2021-08-05 15:19:48 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 wget https://bridges.monash.edu/ndownloader/files/29048439 -O weights.zip
 unzip weights.zip
 rm weights.zip
+wget https://bridges.monash.edu/ndownloader/files/29049003 -O data.zip
+unzip data.zip
+rm data.zip
+
